4a79f9e8756025a8afdb12e0d5601b4a5386f4d6
Nati9 <nataliaespinosa6@gmail.com>
2022-07-27 12:49:35 -0300

    listo

--- Backend/gne/src/main/java/com/portfolio/gne/Controller/CExperiencia.java
+++ Backend/gne/src/main/java/com/portfolio/gne/Controller/CExperiencia.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("explab")
-@CrossOrigin(origins = "https://nataliaespinosa-ap.web.app")
+@CrossOrigin(origins = "https://nataliaespinosa-ap.web.app/")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
